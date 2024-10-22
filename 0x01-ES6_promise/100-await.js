826822f3982327282403e6f1d0450e0c0f0deb66
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-22 15:12:18 +0100

    Advanced Task 1

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,19 @@
+import uploadPhoto from "./5-photo-reject";
+import { createUser } from "./utils";
+
+async function asyncUploadUser() {
+  try {
+    const photoResponse = await uploadPhoto();
+    const userResponse = await createUser();
+
+    return {
+      photo: photoResponse,
+      user: userResponse,
+    };
+  } catch (error) {
+    return  {
+      photo: null,
+      user: null,
+    };
+  }
+}
