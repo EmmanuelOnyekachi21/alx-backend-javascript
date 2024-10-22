66daf17e7c9582a4c22bda374f243873449996dd
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-22 14:41:53 +0100

    Task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,32 +1,17 @@
-// import signUpUser from './4-user-promise';
-// import uploadPhoto from './5-photo-reject';
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
-// export default function handleProfileSignup(firstName, lastName, fileName) {
-//   const signUpPromise = signUpUser(firstName, lastName);
-//   const uploadPromise = uploadPhoto(fileName);
-
-//   // Return a promise that settles all the promises
-//   return Promise.allSettled([signUpPromise, uploadPromise])
-//     .then((results) => results.map((result) => ({
-//       status: result.status,
-//       value: result.status === 'fulfilled' ? result.value : result.reason,
-//     })));
-// }
-
-import signUpUser from './4-user-promise.js';
-import uploadPhoto from './5-photo-reject.js';
-
-export default async function handleProfileSignup(firstName, lastName, fileName) {
-    // Create the promises for sign-up and photo upload
-    const signUpPromise = signUpUser(firstName, lastName);
-    const uploadPromise = uploadPhoto(fileName);
-    
-    // Use Promise.allSettled to handle both promises
-    const results = await Promise.allSettled([signUpPromise, uploadPromise]);
-    
-    // Map the results to the desired structure
-    return results.map((result) => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason,
-    }));
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const signPromise = signUpUser(firstName, lastName);
+  const uploadPromise = uploadPhoto(fileName);
+  const promises = [signPromise, uploadPromise];
+  return Promise.allSettled(promises).then((result) => {
+    const processedResult = result.map((obj) => {
+      if (obj.status === 'fulfilled') {
+        return { status: obj.status, value: obj.value };
+      }
+      return { status: obj.status, value: obj.reason.message };
+    });
+    return processedResult;
+  });
 }
