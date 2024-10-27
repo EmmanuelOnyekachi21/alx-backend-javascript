a0edc937bcb0c66df51bfa4291330ccd416bae4e
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-27 06:24:10 +0100

    Task 6-ReTouched

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -3,22 +3,14 @@ import Building from './5-building';
 export default class SkyHighBuilding extends Building {
   constructor(sqft, floors) {
     super(sqft);
-    this.floors = floors;
+    this._floors = floors;
   }
 
   get floors() {
     return this._floors;
   }
 
-  set floors(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Must be a number');
-    }
-
-    this._floors = floors;
-  }
-
-  evacuateWarningMessage() {
-    return `Evacuate slowly the ${this.floors} floors`;
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
   }
 }
