3bc5848e87d3a6cfcd1d19c3dc6022ac5929cd9e
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-27 06:58:49 +0100

    Task 10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,24 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    // This line creates and returns a new instance of the class uusing the current instance's properties
+    return new this.constructor(this._brand, this._motor, this._color);
+  }
+
+  get brand() {
+    return this._brand;
+  }
+
+  get motor() {
+    return this._motor;
+  }
+
+  get color() {
+    return this._color;
+  }
+}
