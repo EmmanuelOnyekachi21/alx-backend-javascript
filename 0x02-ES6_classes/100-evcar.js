a3a49646c9ca1965f8f0081413c5195cb5c4b5c2
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-10-27 10:10:04 +0100

    Advanced Task 1

---
+++ 0x02-ES6_classes/100-evcar.js
@@ -0,0 +1,14 @@
+import Car from './10-car';
+
+export default class EVCar extends Car {
+  constructor(brand, motor, color, range) {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  // Override the cloneCar method
+  cloneCar() {
+    // Return a new instance of the Car class, not EVCar
+    return new Car(this._brand, this._motor, this._color);
+  }
+}
