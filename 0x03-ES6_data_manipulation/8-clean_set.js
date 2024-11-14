581235d3db9a77a36f920fe6f31aca8288bfb373
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-11-14 14:52:07 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,4 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
-    return '';
-  }
-
   const filtered = [...set]
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length));
