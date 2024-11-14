e26b3d52dd83c291aa1d431cf0f0969eef5a2aa9
Emmanuel Onyekachi <emmanuelonyekachi04122000@gmail.com>
2024-11-14 15:07:18 +0100

    Task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,7 +1,12 @@
 export default function cleanSet(set, startString) {
+  // Convert the set to an array, filter values that start with startString
+  if (startString === '') {
+    return '';
+  }
   const filtered = [...set]
-    .filter((value) => value.startsWith(startString))
-    .map((value) => value.slice(startString.length));
+    .filter((value) => value.startsWith(startString)) // Keep values that start with startString
+    .map((value) => value.slice(startString.length)); // Remove the startString part of each value
 
+  // Join the remaining parts of each value with a hyphen and return it
   return filtered.join('-');
 }
